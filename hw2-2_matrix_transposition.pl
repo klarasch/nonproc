4cc10b122c0eca2b3af9cb8a0b489579aa86b91a
Klára Scholleová <klara@ksch.cz>
2017-03-20 00:35:00 +0100

    Update description

--- hw2-2_matrix_transposition.pl
+++ hw2-2_matrix_transposition.pl
@@ -1,14 +1,14 @@
 % Matrix Transposition
 % ====================
-%
 % transp(+M, ?R)
+%
 % Transposes any valid rectangular matrix M (represented as a list of
 % lists) into a matrix R.
 %
-% Core idea of the program below is that the program determines a column
-% by column (rows in the result) in the original matrix by going row by
-% row in matrixes which don't have the columns which were already dealt
-% with.
+% The core idea of the program below is that the program determines column
+% by column in the original matrix and turns them into rows in the result.
+% That is done by taking first elements in rows of input matrix, then
+% the first column in the input is "deleted", and the process is repeated. 
 
 % Transposition of an empty matrix is an empty matrix.
 transp([], []) :- !.
@@ -20,11 +20,13 @@ transp(M, [FirstCol | R]) :-
     firstcol(M, FirstCol, RestRows), !,
     transp(RestRows, R).
 
-% Predicate firstcol(+Rows, ?Col, ?RestRows) is satisfied,
+% Predicate >>>firstcol(+Rows, ?Col, ?RestRows)<<< is satisfied,
 % if Col is the first column in the matrix Rows and RestRows are rows
-% in the matrix Rows except for the first ("returns" the first column)
+% in the matrix Rows except for the first ("returns" the first column).
+
 firstcol([[X | Row] | Rows], [X| Col], [Row | RestRows]) :- !,
     firstcol(Rows, Col, RestRows).
 
 % First column of an empty matrix is an empty column (= empty matrix).
+
 firstcol([], [], []).
